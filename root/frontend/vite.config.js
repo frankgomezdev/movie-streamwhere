14d3b67394d553ffc420f8ccae2fb77990dd67bc
Franklin Gomez <franklingomezdev@gmail.com>
2025-03-19 23:26:32 -0400

    more updates to connect render web service to static site.

--- root/frontend/vite.config.js
+++ root/frontend/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import { API_URL } from './config';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: API_URL,
+      "/api": {
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
