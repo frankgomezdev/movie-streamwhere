dad29146c368b3cdb3f8f1341d945effc8b28d1a
Franklin Gomez <franklingomezdev@gmail.com>
2025-03-19 23:31:41 -0400

    changed api_url config

--- root/frontend/vite.config.js
+++ root/frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_URL,
+        target: import.meta.env.VITE_API_URL || "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
